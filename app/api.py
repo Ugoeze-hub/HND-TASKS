fc495ac1bd3db948af006266129dc426b68db80e
Ugoeze-hub <eluchieugoeze@gmail.com>
2025-10-19 13:57:33 +0100

    Ready to deploy 15

--- app/api.py
+++ app/api.py
@@ -45,7 +45,5 @@ def hng_task_0():
     json_data = json.dumps(data, indent=2, ensure_ascii=False)
     return Response(json_data, mimetype='application/json')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
 
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,5 @@
-from api import app
+from api import *
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
