2fb5bfde03419031c7566fe572effe468bc13294
Ugoeze-hub <eluchieugoeze@gmail.com>
2025-10-19 10:44:17 +0100

    Ready to deploy 8

--- app/api.py
+++ app/api.py
@@ -51,5 +51,5 @@ def hng_task_0():
 # if __name__ == '__main__':
 #     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
 
